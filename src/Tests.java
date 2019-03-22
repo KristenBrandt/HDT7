8b41898ef99e5d2c55d502321538e1bd7b32c7b2
KristenBrandt <kabrandtf@gmail.com>
2019-03-21 23:29:57 -0600

    Unit Tests

---
+++ src/Tests.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Tests {
+
+    @Test
+    public void insert() {
+    }
+
+    @Test
+    public void inorder() {
+    }
+
+    @Test
+    public void getValue() {
+    }
+
+    @Test
+    public void getKey() {
+    }
+}
